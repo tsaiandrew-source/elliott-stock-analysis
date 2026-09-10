2ac3c23aaa7886f9b8f0fa5b86cd9b4e39ee71bb
tsaiandrew-source <tsaiandrew-source@users.noreply.github.com>
2026-09-10 14:01:41 -0700

    Compact inline digest sources

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'elliott-pwa-v14';
+const CACHE_VERSION = 'elliott-pwa-v15';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const APP_ROOT = new URL('./', self.location);
