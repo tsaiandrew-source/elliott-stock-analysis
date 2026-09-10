1727c2fa1aaa30d446a901b90a0d0c2d50e85985
tsaiandrew-source <tsaiandrew-source@users.noreply.github.com>
2026-09-10 13:47:55 -0700

    Simplify digest calendar rows

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'elliott-pwa-v8';
+const CACHE_VERSION = 'elliott-pwa-v9';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const APP_ROOT = new URL('./', self.location);
